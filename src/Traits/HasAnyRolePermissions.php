1b2cbc37452402742f55864fde9dedeb2bcf26c2
Waad Mawlood <44348636+waadmawlood@users.noreply.github.com>
2023-03-30 11:40:09 +0300

    Fix single guard to multi guard in HasAnyRolePermissions trait

--- src/Traits/HasAnyRolePermissions.php
+++ src/Traits/HasAnyRolePermissions.php
@@ -9,22 +9,23 @@ trait HasAnyRolePermissions
     /**
      * has Any Role or Permissions
      * @param string|array|null $role_or_permission
-     * @param string|null $guard
+     * @param array|string|null $guard
      * @return bool
      */
-    public function hasAnyRolePermissions(string|array|null $role_or_permission = null, string|null $guard = null)
+    public function hasAnyRolePermissions(string|array|null $role_or_permission = null, array|string|null $guard = null)
     {
         if (blank($role_or_permission))
             return false;
 
         $guard = $guard ?: config('auth.defaults.guard');
+        $guard = Arr::wrap($guard);
 
         $role_or_permission = Arr::wrap($role_or_permission);
 
-        if ($this->roles->whereIn('name', $role_or_permission)->where('guard_name', $guard)->count() > 0)
+        if ($this->roles->whereIn('name', $role_or_permission)->whereIn('guard_name', $guard)->count() > 0)
             return true;
 
-        if ($this->getAllPermissions()->whereIn('name', $role_or_permission)->where('guard_name', $guard)->count() > 0)
+        if ($this->getAllPermissions()->whereIn('name', $role_or_permission)->whereIn('guard_name', $guard)->count() > 0)
             return true;
 
         return false;
